2264c0c121fa4b8eb20decee335d6153e90a6454
Lauren <lecavs91@msn.com>
2021-08-09 20:25:14 -0700

    wrote constructor and initialize censusMatrix

--- src/queryresponders/ComplexSequential.java
+++ src/queryresponders/ComplexSequential.java
@@ -5,15 +5,65 @@ import cse332.interfaces.QueryResponder;
 import cse332.types.*;
 import cse332.exceptions.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class ComplexSequential extends QueryResponder {
+    int[][] censusMatrix;
+    MapCorners map;
+    int numRows;
+    int numColumns;
 
     public ComplexSequential(CensusGroup[] censusData, int numColumns, int numRows) {
-        throw new NotYetImplementedException();
+        if(censusData == null) { throw new IllegalArgumentException(); }
+        this.numRows = numRows;
+        this.numColumns = numColumns;
+
+        //pre-processing step 1: initialize corners of map and total population
+        this.map = new MapCorners(censusData[0]);
+        int population = 0;
+        for(CensusGroup group: censusData) {
+            map = map.encompass(new MapCorners(group));
+            population += group.population;
+        }
+        this.totalPopulation = population;
+
+        //pre-processing step 2: create 2D array to store population at location indexes
+        int[][] censusMatrix = new int[numRows][numColumns];
+        double xScale = (map.north - map.south) / (numColumns - 1);
+        double yScale = (map.east - map.west) / (numRows - 1);
+        double xShift = map.south * -1;
+        double yShift = map.west * -1;
+
+        for(CensusGroup group: censusData) {
+            int column = (int) ((group.latitude + xShift ) /xScale) ;
+            int row = (int) ((group.longitude + yShift) /yScale) ;
+            censusMatrix[row][column] = group.population;
+
+        }
+
+        for(int i = 0; i<numRows; i++)
+        {
+            for(int j = 1; j<numColumns; j++)
+            {
+                censusMatrix[i][j] += censusMatrix[i][j-1];
+            }
+        }
+
+        for(int j = 0; j < numColumns; j++) {
+            for(int i = numRows-2; i>=0; i--)
+            {
+                censusMatrix[i][j] += censusMatrix[i + 1][j];
+            }
+        }
+
+        this.censusMatrix = censusMatrix;
+
     }
 
     @Override
     public int getPopulation(int west, int south, int east, int north) {
-        throw new NotYetImplementedException();
+        return 0;
     }
 }
