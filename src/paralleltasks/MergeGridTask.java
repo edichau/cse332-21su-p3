08282a276c57d4b80da6a31882126871a5df2559
BuildTools <unconfigured@null.spigotmc.org>
2021-08-10 21:52:40 -0700

    finished MergeGridTask.java but no tests so idk

--- src/paralleltasks/MergeGridTask.java
+++ src/paralleltasks/MergeGridTask.java
@@ -15,16 +15,37 @@ public class MergeGridTask extends RecursiveAction {
     final static int SEQUENTIAL_CUTOFF = 10;
 
     public MergeGridTask(int[][] left, int[][] right, int rowLo, int rowHi, int colLo, int colHi) {
-        throw new NotYetImplementedException();
+        this.left = left;
+        this.right = right;
+        this.rowLo = rowLo;
+        this.rowHi = rowHi;
+        this.colLo = colLo;
+        this.colHi = colHi;
     }
 
     @Override
     protected void compute() {
-        throw new NotYetImplementedException();
+        if ((rowHi - rowLo) <= SEQUENTIAL_CUTOFF || (colHi - colLo) <= SEQUENTIAL_CUTOFF) {
+            sequentialMergeGird(rowLo, rowHi, colLo, colHi);
+        } else {
+
+            int mid = rowLo + (rowHi - rowLo) / 2;
+
+            MergeGridTask leftRow = new MergeGridTask(left, right, rowLo, mid, colLo, colHi);
+            MergeGridTask rightCol = new MergeGridTask(left, right, mid, rowHi, colLo, colHi);
+
+            leftRow.fork();
+            rightCol.compute();
+            leftRow.join();
+        }
     }
 
     // according to google gird means "prepare oneself for something difficult or challenging" so this typo is intentional :)
-    private void sequentialMergeGird() {
-        throw new NotYetImplementedException();
+    private void sequentialMergeGird(int rowLo, int rowHi, int colLo, int colHi) {
+        for (int i = colLo; i < colHi; i++) {
+            for (int j = rowLo; j < rowHi; j++) {
+                left[i][j] += right[i][j];
+            }
+        }
     }
 }
