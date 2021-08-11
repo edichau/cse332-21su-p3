9327ca7a93e18a5c0ad7c7aba34267edb208e6b2
Lauren <lecavs91@msn.com>
2021-08-10 21:28:09 -0700

    Passes Tests

--- src/queryresponders/ComplexSequential.java
+++ src/queryresponders/ComplexSequential.java
@@ -30,40 +30,71 @@ public class ComplexSequential extends QueryResponder {
         this.totalPopulation = population;
 
         //pre-processing step 2: create 2D array to store population at location indexes
-        int[][] censusMatrix = new int[numRows][numColumns];
-        double xScale = (map.north - map.south) / (numColumns - 1);
-        double yScale = (map.east - map.west) / (numRows - 1);
-        double xShift = map.south * -1;
-        double yShift = map.west * -1;
+        int[][] censusMatrix = new int[numRows + 1][numColumns + 1];
+        double yScale = ( map.north - map.south) / (numRows );
+        double xScale = (map.east - map.west) / (numColumns );
+        double yShift = map.south * -1;
+        double xShift = map.west  * -1;
 
+        //put censusData into table such that:
+        //        [1,1](S,W)-------> (S,E) [1,numColumns]
+        //
+        //
+        //
+        //[numRows, 1] (N,W)-------> (N,E) [numRows, numColumns]
         for(CensusGroup group: censusData) {
-            int column = (int) ((group.latitude + xShift ) /xScale) ;
-            int row = (int) ((group.longitude + yShift) /yScale) ;
-            censusMatrix[row][column] += group.population;
+            int row = (int) Math.floor(((group.latitude + yShift) /yScale) + 1);
+            int column = (int) Math.floor(((group.longitude + xShift)/ xScale) + 1) ;
 
+            //if row or column is on outermost boarder it goes into max row/column
+            if(row > numRows) { row = numRows; }
+            if(column > numColumns) { column = numColumns; }
+
+            censusMatrix[row][column] += group.population;
         }
 
-        for(int i = 0; i<numRows; i++)
+        //add indexes west to east
+        for(int y = 1; y<=numRows; y++)
         {
-            for(int j = 1; j<numColumns; j++)
+            for(int x = 2; x <= numColumns; x++)
             {
-                censusMatrix[i][j] += censusMatrix[i][j-1];
+                censusMatrix[y][x] += censusMatrix[y][x-1];
             }
         }
 
-        for(int j = 0; j < numColumns; j++) {
-            for(int i = numRows-2; i>=0; i--)
+        //add indexes south to north
+        for(int x = 1; x <= numColumns; x++) {
+            for(int y = 2; y <= numRows; y++)
             {
-                censusMatrix[i][j] += censusMatrix[i + 1][j];
+                censusMatrix[y][x] += censusMatrix[y-1][x];
             }
         }
 
         this.censusMatrix = censusMatrix;
-
     }
 
     @Override
     public int getPopulation(int west, int south, int east, int north) {
-        return 0;
+        if(!validParameters(west, south, east, north)) { throw new IllegalArgumentException(); }
+
+        int indexPop = censusMatrix[north][east];
+        int subSE = 0;
+        if(south - 1 > 0) {subSE = censusMatrix[south - 1][east];}
+        int subNW = 0;
+        if(west - 1 > 0) { subNW = censusMatrix[north][west -1];}
+        int addSW = 0;
+        if(south - 1 > 0 && west - 1 > 0) { addSW= censusMatrix[south-1][west - 1];}
+
+        return indexPop + addSW - (subSE + subNW);
     }
+
+    private boolean validParameters(int west, int south, int east, int north) {
+        if(west < 1 || west > numColumns) { return false; }
+        if(south < 1 || south > numRows) { return false; }
+        if(east < west || east > numColumns) { return false; }
+        if(north < south || north > numRows) { return false; }
+
+        return true;
+    }
+
 }
